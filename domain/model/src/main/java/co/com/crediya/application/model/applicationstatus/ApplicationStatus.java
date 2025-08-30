f8f6e91b0edf00e1fd5662d6a7d6d7132de74af1
Juan Urrego <juan.u@hyros.com>
2025-08-29 22:19:43 -0400

    Add application status entity and gateway

---
+++ domain/model/src/main/java/co/com/crediya/application/model/applicationstatus/ApplicationStatus.java
@@ -0,0 +1,20 @@
+package co.com.crediya.application.model.applicationstatus;
+
+import java.util.UUID;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder(toBuilder = true)
+public class ApplicationStatus {
+  private UUID id;
+  private String name;
+  private String description;
+}
