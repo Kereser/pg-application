00797012b0290172af898d3d6fd79feec55a56af
Juan Urrego <juan.u@hyros.com>
2025-08-31 19:37:19 -0400

    Formatting/renaming

--- infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/application/api/dto/CreateApplicationDTORequest.java
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/crediya/application/api/dto/CreateApplicationDTORequest.java
@@ -1,12 +1,12 @@
 package co.com.crediya.application.api.dto;
 
+import java.math.BigDecimal;
+
 import jakarta.annotation.Nonnull;
 import jakarta.validation.constraints.NotBlank;
 
-import java.math.BigDecimal;
-
 public record CreateApplicationDTORequest(
-        @Nonnull String idNumber,
-        @Nonnull BigDecimal amount,
-        @Nonnull Integer applicationPeriod,
-        @Nonnull @NotBlank String productName) {}
+    @Nonnull String idNumber,
+    @Nonnull BigDecimal amount,
+    @Nonnull Integer applicationPeriod,
+    @Nonnull @NotBlank String productName) {}
