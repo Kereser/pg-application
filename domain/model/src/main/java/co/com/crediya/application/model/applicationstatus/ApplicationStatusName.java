bfd01e103717c87f439a363282953871d38f3f9c
Juan Urrego <juan.u@hyros.com>
2025-09-03 23:00:34 -0400

    Update app status to use enum for name

---
+++ domain/model/src/main/java/co/com/crediya/application/model/applicationstatus/ApplicationStatusName.java
@@ -0,0 +1,45 @@
+package co.com.crediya.application.model.applicationstatus;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import co.com.crediya.application.model.exceptions.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum ApplicationStatusName {
+  PENDING("PENDING"),
+  REJECTED("REJECTED"),
+  MANUAL_REVISION("MANUAL_REVISION");
+
+  private final String name;
+
+  private static final Map<String, ApplicationStatusName> BY_NAME = new HashMap<>();
+
+  static {
+    for (ApplicationStatusName n : values()) {
+      BY_NAME.put(n.name, n);
+    }
+  }
+
+  public static ApplicationStatusName fromName(String name) {
+    if (name == null) {
+      throw new MissingValueOnRequiredFieldException(
+          Fields.APPLICATION_STATUS_NAME.getName(), PlainErrors.NOT_EMPY.getName());
+    }
+
+    String nameUpp = name.toUpperCase();
+    ApplicationStatusName statusName = BY_NAME.get(nameUpp);
+
+    if (statusName == null) {
+      throw new IllegalValueForArgumentException(
+          Fields.APPLICATION_STATUS_NAME.getName(),
+          TemplateErrors.X_NOT_VALID_FORMAT_FOR_Y.buildMsg(
+              nameUpp, Fields.APPLICATION_STATUS_NAME.getName()));
+    }
+
+    return statusName;
+  }
+}
