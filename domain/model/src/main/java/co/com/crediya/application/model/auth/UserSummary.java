3d498eff878acaf354b58efc82bf356a3fd56aa7
Juan Urrego <juan.u@hyros.com>
2025-09-03 23:00:49 -0400

    Add base salary to user summary

--- domain/model/src/main/java/co/com/crediya/application/model/auth/UserSummary.java
+++ domain/model/src/main/java/co/com/crediya/application/model/auth/UserSummary.java
@@ -1,6 +1,12 @@
 package co.com.crediya.application.model.auth;
 
+import java.math.BigDecimal;
 import java.util.UUID;
 
 public record UserSummary(
-    UUID id, String email, String firstName, String idType, String idNumber) {}
+    UUID id,
+    String email,
+    BigDecimal baseSalary,
+    String firstName,
+    String idType,
+    String idNumber) {}
--- domain/model/src/main/java/co/com/crediya/application/model/auth/gateway/AuthGateway.java
+++ domain/model/src/main/java/co/com/crediya/application/model/auth/gateway/AuthGateway.java
@@ -1,8 +1,14 @@
 package co.com.crediya.application.model.auth.gateway;
 
+import java.util.Set;
+import java.util.UUID;
+
 import co.com.crediya.application.model.auth.UserSummary;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface AuthGateway {
   Mono<UserSummary> findUserByIdNumber(String idNumber);
+
+  Flux<UserSummary> findUsersByIdIn(Set<UUID> userIds);
 }
