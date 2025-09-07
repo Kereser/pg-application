095d116dc681f8ed8fc91f7d2df3e82b41efd267
Juan Urrego <juan.u@hyros.com>
2025-09-07 13:09:05 -0400

    New values for entities and application status

--- domain/model/src/main/java/co/com/crediya/application/model/exceptions/TemplateErrors.java
+++ domain/model/src/main/java/co/com/crediya/application/model/exceptions/TemplateErrors.java
@@ -5,6 +5,7 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public enum TemplateErrors {
   X_NOT_FOUND_FOR_Y("%s was not found for %s"),
+  X_ALREADY_ASSIGNED_FOR_Y("%s is already assigned for %s"),
   X_NOT_VALID_VALUE_FOR_Y_WITH_RANGE("%s not valid value for %s. Valid ranges: %s to %s"),
   X_NOT_VALID_FORMAT_FOR_Y("Value %s does not meet format condition for %s field"),
   LENGTH_BOUNDARIES("Value must be between %s and %s"),
