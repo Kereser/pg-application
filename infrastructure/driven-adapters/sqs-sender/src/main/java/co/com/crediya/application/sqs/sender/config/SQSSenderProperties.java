6856bec00c3c23671a80e4a633ff698ac6dac5e0
Juan Urrego <juan.u@hyros.com>
2025-09-09 14:56:36 -0400

    Update properties to get debt evaluation queue url

--- infrastructure/driven-adapters/sqs-sender/src/main/java/co/com/crediya/application/sqs/sender/config/SQSSenderProperties.java
+++ infrastructure/driven-adapters/sqs-sender/src/main/java/co/com/crediya/application/sqs/sender/config/SQSSenderProperties.java
@@ -5,4 +5,5 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import co.com.crediya.application.model.CommonConstants;
 
 @ConfigurationProperties(prefix = CommonConstants.ConfigProperties.ADAPTER_SQS)
-public record SQSSenderProperties(String region, String queueUrl, String endpoint) {}
+public record SQSSenderProperties(
+    String region, String notificationsQueue, String debtEvaluationQueue, String endpoint) {}
