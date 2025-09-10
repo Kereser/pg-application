9f8e4437a68b574eb79ff2763e662e217a4c4f6f
Juan Urrego <juan.u@hyros.com>
2025-09-09 23:09:27 -0400

    method on event publish to handle debt evaluation queue

--- domain/model/src/main/java/co/com/crediya/application/model/eventpublisher/gateway/NotificationEventPublisher.java
+++ domain/model/src/main/java/co/com/crediya/application/model/eventpublisher/gateway/NotificationEventPublisher.java
@@ -1,8 +1,11 @@
 package co.com.crediya.application.model.eventpublisher.gateway;
 
+import co.com.crediya.application.model.eventpublisher.dto.DebtEvaluationDTO;
 import co.com.crediya.application.model.sqs.SqsSummaryDTO;
 import reactor.core.publisher.Mono;
 
 public interface NotificationEventPublisher {
   Mono<Void> publishStatusUpdate(SqsSummaryDTO sqsSummaryDTO);
+
+  Mono<Void> publishDebtEvaluationQueue(DebtEvaluationDTO debtEvaluationDTO);
 }
