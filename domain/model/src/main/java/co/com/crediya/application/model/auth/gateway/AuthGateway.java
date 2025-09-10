93ebb140feeba9750a5816394a1d54d49f01b38b
Juan Urrego <juan.u@hyros.com>
2025-09-09 23:11:06 -0400

    Update param type

--- domain/model/src/main/java/co/com/crediya/application/model/auth/gateway/AuthGateway.java
+++ domain/model/src/main/java/co/com/crediya/application/model/auth/gateway/AuthGateway.java
@@ -3,12 +3,13 @@ package co.com.crediya.application.model.auth.gateway;
 import java.util.Set;
 import java.util.UUID;
 
+import co.com.crediya.application.model.application.vo.IdNumber;
 import co.com.crediya.application.model.auth.UserSummary;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface AuthGateway {
-  Mono<UserSummary> findUserByIdNumber(String idNumber);
+  Mono<UserSummary> findUserByIdNumber(IdNumber idNumber);
 
   Flux<UserSummary> findUsersByIdIn(Set<UUID> userIds);
 }
